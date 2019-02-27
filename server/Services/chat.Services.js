b3dc025d3467aba980552746ac12fdc4ca55a66d
BhavanaSai1506 <bhavanab1506@gmail.com>
2019-02-27 10:37:10 +0530

    chatApp

---
+++ server/Services/chat.Services.js
@@ -0,0 +1,22 @@
+const chatModel = require('../Apllication/Model/chat.Model')
+exports.addMessage = (req, callback) => {
+    chatModel.addMessage(req, (err, result) => {
+        if (err) {
+
+            return callback(err);
+        } else {
+
+            return callback(null, result);
+        }
+    })
+}
+exports.getAllUserChats = (req, callback) => {
+    chatModel.getAllUserChats(req, (err, result) => {
+        if (err) {
+            callback(err);
+        } else {
+            callback(null, result);
+        }
+    })
+}
+
